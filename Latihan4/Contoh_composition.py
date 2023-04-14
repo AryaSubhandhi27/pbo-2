fbdb578e7695da01c3f1cff6aefa19379d3ad463
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-04-15 04:24:42 +0700

    Create Contoh_composition.py

---
+++ Latihan4/Contoh_composition.py
@@ -0,0 +1,20 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+class Pekerja:
+    def __init__(self, nama, umur):
+        self.nama = nama
+        self.umur = umur
+        
+class Perusahaan:
+    def __init__(self, nama, pekerja):
+        self.nama = nama
+        self.pekerja = pekerja
+    def daftar_pekerja(self):
+        for pekerja in self.pekerja:
+            print(pekerja.nama, pekerja.umur)
+pekerja1 = Pekerja("Andi", 25)
+pekerja2 = Pekerja("Budi", 30)
+perusahaan = Perusahaan("ABC", [pekerja1, pekerja2])
+perusahaan.daftar_pekerja()
