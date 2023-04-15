749d6a468b66ae714c700f5272a653113dde2882
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-04-15 14:39:28 +0700

    Create Composition1.py

---
+++ Tugas4/Composition1.py
@@ -0,0 +1,21 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C 
+
+class House:
+    def __init__(self, area):
+        self.area = area
+
+class Apartment:
+    def __init__(self, area):
+        self.area = area
+
+class Building:
+    def __init__(self, house, apartment):
+        self.house = house
+        self.apartment = apartment
+house = House("72 square meters")
+apartment = Apartment("30 square meters")
+building = Building(house, apartment)
+print(building.house.area) # output: 8
+print(building.apartment.area) # output: 500
