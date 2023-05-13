52b1e286a70efc5f82f54c9b64dfdfcef24c9952
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:29:48 +0700

    Create AssertionError.py

---
+++ Tugas5/AssertionError.py
@@ -0,0 +1,11 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+try:
+    x = 1
+    y = 0
+    assert y !=0, "Invalid Operation"
+    print(x/y)
+except AssertionError as a:
+    print(a)
