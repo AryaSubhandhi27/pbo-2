32d621db8fe149f2f7688a839e52a9995d638d42
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:44:13 +0700

    Create SyntaxError.py

---
+++ Tugas5/SyntaxError.py
@@ -0,0 +1,15 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+# try :
+#     print("hello")
+# except SyntaxError as e:
+#     print("Invalid Syntax")
+# except :
+#     print("Invalid Syntax")
+
+try:
+    eval("hello =")
+except SyntaxError:
+    print("Invalid Syntax")
