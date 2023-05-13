d4684969009293e23500936ea92612a4866270fb
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:12:02 +0700

    Create TypeError.py

---
+++ Pratikum5/TypeError.py
@@ -0,0 +1,11 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+
+x = 5
+y = "Hello"
+try:
+    z = x + y
+except TypeError:
+    print("Error : cannot add an int and a str")
