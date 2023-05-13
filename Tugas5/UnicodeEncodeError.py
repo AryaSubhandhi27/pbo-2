7331c04781f83af3937473712c0c3c936f08ed3c
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:46:07 +0700

    Create UnicodeEncodeError.py

---
+++ Tugas5/UnicodeEncodeError.py
@@ -0,0 +1,9 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+a = u'café'
+b = a.encode('utf8')
+r = str(b)
+print("The unicode string after fixing the UnicodeEncodeError is as follows:")
+print(r)
