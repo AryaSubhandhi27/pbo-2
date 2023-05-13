3d0ec555ea7674c3d413c3a3caa9464dde5af0a1
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 11:45:33 +0700

    Create contoh4_threading.py

---
+++ Latihan6/contoh4_threading.py
@@ -0,0 +1,19 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C 
+
+import threading
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada thread baru"""
+    print('Worker', num)
+if __name__ == '__main__':
+    # Membuat list untuk menyimpan setiap thread
+    threads = []
+    # Memulai setiap thread dengan menggunakan threading.Thread
+    for i in range(5):
+        t = threading.Thread(target=worker, args=(i,))
+        t.start()
+        threads.append(t)
+    # Menunggu thread selesai
+    for t in threads:
+        t.join()
