e2e728c072a4798395c96e3d417275ee9df05e5d
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:30:54 +0700

    Create EOFError.py

---
+++ Tugas5/EOFError.py
@@ -0,0 +1,11 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+try:
+    value1 = int(input("enter value 1 :"))
+    value2 = int(input("enter value 2 :"))
+    result = value1 + value2
+    print(f"The sum of {value1} and {value2} is: {result}")
+except EOFError as e:
+    print("End-Of-File when reading input")
