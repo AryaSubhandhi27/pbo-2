78f4eca56fd72451c14fda77ca6e65f729d306e1
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 11:54:12 +0700

    Create contoh9_ray.py

---
+++ Latihan6/contoh9_ray.py
@@ -0,0 +1,16 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C 
+
+import ray
+@ray.remote
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada proses multiprocessing"""
+    print('Worker', num)
+if __name__ == '__main__':
+    # Inisialisasi Ray
+    ray.init()
+    # Buat task untuk setiap item secara parallel
+    results = [worker.remote(i) for i in range(5)]
+    # Ambil hasil task secara parallel
+    ray.get(results)
