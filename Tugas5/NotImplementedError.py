a35c64ace52e31bb41b909e04a399f1187a4c89c
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 13:33:37 +0700

    Create NotImplementedError.py

---
+++ Tugas5/NotImplementedError.py
@@ -0,0 +1,9 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+try:
+    salary = int(input("Enter salary: "))
+    print("So cool you earn %d amount." % salary)
+except NotImplementedError:
+    print("Hey, that wasn't a number!")
