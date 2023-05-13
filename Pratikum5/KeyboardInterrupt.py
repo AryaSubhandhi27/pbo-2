0a3536ab84e70789e265cbe41496c1744470c0a6
Arya Subhandhi <127746691+AryaSubhandhi27@users.noreply.github.com>
2023-05-13 12:50:45 +0700

    Create KeyboardInterrupt.py

---
+++ Pratikum5/KeyboardInterrupt.py
@@ -0,0 +1,11 @@
+# Nama    : Arya Subhandhi
+# NIM     : 210511107
+# Kelas   : R3/TI21C
+
+try:
+    x = input()
+    print ('Try using KeyboardInterrupt')
+except KeyboardInterrupt:
+    print ('KeyboardInterrupt exception is caught')
+else:
+    print ('No exceptions are caught')
